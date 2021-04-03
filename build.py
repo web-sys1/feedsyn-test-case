4e1d14e67adb5f49b13879720ee9a18775e130bd
Web Source (SRC) <61905565+web-sys1@users.noreply.github.com>
2021-04-03 16:32:38 +0300

    Update and rename build.sh to build.py

---
+++ build.py
@@ -0,0 +1,20 @@
+import os
+import NFSyndication
+import subprocess
+from NFSyndication import main as NFS_init
+
+def entry_point():
+    """ We use these conditions to check the statement"""
+    subscriptions = [
+     'http://feedpress.me/512pixels',
+     'http://www.leancrew.com/all-this/feed/',
+     'http://ihnatko.com/feed/',
+     'http://blog.ashleynh.me/feed']
+  
+    with open(f'feeds.txt', 'w', encoding='utf8') as f:
+     f.write(",".join(subscriptions).replace(',', '\n'))
+    return NFS_init()
+
+
+"""Then initialize code."""
+entry_point()
