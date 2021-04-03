513a8a1f8bd894c6071f577fad564c679b1273a5
Web Source (SRC) <web.system.management@gmail.com>
2021-04-03 23:24:40 +0300

    Update build.py

--- build.py
+++ build.py
@@ -1,20 +1,14 @@
-import os
-import NFSyndication
-import subprocess
-from NFSyndication import main as NFS_init
+from NFSyndication import __main__ as NFS_init
 
 def entry_point():
     """ We use these conditions to check the statement"""
     subscriptions = [
-     'http://feedpress.me/512pixels',
-     'http://www.leancrew.com/all-this/feed/',
-     'http://ihnatko.com/feed/',
-     'http://blog.ashleynh.me/feed']
+     'https://www.tvmagia.ro/feed/',
+     ]
   
     with open(f'feeds.txt', 'w', encoding='utf8') as f:
      f.write(",".join(subscriptions).replace(',', '\n'))
-    return NFS_init()
-
+    return NFS_init.run()
 
 """Then initialize code."""
 entry_point()
