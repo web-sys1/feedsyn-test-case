d4586c057b3e0abe656a36032cc47ca036a40537
Web Source (SRC) <web.system.management@gmail.com>
2021-04-04 08:33:49 +0300

    Update build.py

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 import subprocess
+from NFSyndication import __main__ as NFS_init
 
 def entry_point():
    """ We use these conditions to check the statement"""
@@ -16,6 +17,7 @@ def entry_point():
      ]
    with open(f'feeds.txt', 'w', encoding='utf8') as f:
      f.write(",".join(subscriptions).replace(',', '\n'))
+   return NFS_init.run()
 
 """Then initialize code."""
 entry_point()
