17755137ccc2f26936b393bf3074502d7ae21f6a
Web Source (SRC) <web.system.management@gmail.com>
2021-04-04 08:20:23 +0300

    Update build.py

--- build.py
+++ build.py
@@ -1,14 +1,21 @@
-from NFSyndication import __main__ as NFS_init
+import subprocess
 
 def entry_point():
-    """ We use these conditions to check the statement"""
-    subscriptions = [
-     'https://www.tvmagia.ro/feed/',
+   """ We use these conditions to check the statement"""
+   subscriptions = [
+     'http://feeds.feedburner.com/jblanton',
+     'http://feeds.feedburner.com/IgnoreTheCode',
+     'http://indiestack.com/feed/',
+     'http://feedpress.me/inessential',
+     'http://feeds.feedburner.com/JamesFallows',
+     'http://feeds.feedburner.com/theendeavour',
+     'http://feed.katiefloyd.me/',
+     'http://feeds.feedburner.com/KevinDrum',
+     'http://www.kungfugrippe.com/rss',
+     'http://lancemannion.typepad.com/lance_mannion/rss.xml',
      ]
-  
-    with open(f'feeds.txt', 'w', encoding='utf8') as f:
+   with open(f'feeds.txt', 'w', encoding='utf8') as f:
      f.write(",".join(subscriptions).replace(',', '\n'))
-    return NFS_init.run()
 
 """Then initialize code."""
 entry_point()
