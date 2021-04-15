48062cbc3325d53f3fb26004a95723f58a550774
Web Source (SRC) <web.system.management@gmail.com>
2021-04-15 13:00:09 +0300

    Update build.py

--- build.py
+++ build.py
@@ -6,15 +6,7 @@ def entry_point():
    """ We use these conditions to check the statement"""
    subscriptions = [
      'http://feeds.feedburner.com/jblanton',
-     'http://feeds.feedburner.com/IgnoreTheCode',
-     'http://indiestack.com/feed/',
-     'http://feedpress.me/inessential',
-     'http://feeds.feedburner.com/JamesFallows',
-     'http://feeds.feedburner.com/theendeavour',
-     'http://feed.katiefloyd.me/',
-     'http://feeds.feedburner.com/KevinDrum',
-     'http://www.kungfugrippe.com/rss',
-     'http://lancemannion.typepad.com/lance_mannion/rss.xml',
+     'http://feeds.feedburner.com/IgnoreTheCode'
      ]
    with open(f'feeds.txt', 'w', encoding='utf8') as f:
      f.write(",".join(subscriptions).replace(',', '\n'))
