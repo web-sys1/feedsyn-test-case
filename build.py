1672edf1ddef08753841f27ac75f536c89b41752
Web Source (SRC) <web.system.management@gmail.com>
2021-04-15 10:41:33 +0300

    Update build.py

--- build.py
+++ build.py
@@ -7,13 +7,6 @@ def entry_point():
    subscriptions = [
      'http://feeds.feedburner.com/jblanton',
      'http://feeds.feedburner.com/IgnoreTheCode',
-     'http://indiestack.com/feed/',
-     'http://feedpress.me/inessential',
-     'http://feeds.feedburner.com/JamesFallows',
-     'http://feeds.feedburner.com/theendeavour',
-     'http://feeds.feedburner.com/KevinDrum',
-     'http://www.kungfugrippe.com/rss',
-     'http://lancemannion.typepad.com/lance_mannion/rss.xml',
      ]
    with open(f'feeds.txt', 'w', encoding='utf8') as f:
      f.write(",".join(subscriptions).replace(',', '\n'))
