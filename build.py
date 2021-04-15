a285cb3b08e613b24fc2d99d2696e1d06a99337a
Web Source (SRC) <web.system.management@gmail.com>
2021-04-15 12:59:11 +0300

    Update build.py

--- build.py
+++ build.py
@@ -1,18 +1,24 @@
 import subprocess
-from NFSyndication.extras import exist_template
+from NFSyndication import __main__ as NFS_init
 from NFSyndication import styles
 
-"""
 def entry_point():
-  #We use these conditions to check the statement
+   """ We use these conditions to check the statement"""
    subscriptions = [
      'http://feeds.feedburner.com/jblanton',
      'http://feeds.feedburner.com/IgnoreTheCode',
+     'http://indiestack.com/feed/',
+     'http://feedpress.me/inessential',
+     'http://feeds.feedburner.com/JamesFallows',
+     'http://feeds.feedburner.com/theendeavour',
+     'http://feed.katiefloyd.me/',
+     'http://feeds.feedburner.com/KevinDrum',
+     'http://www.kungfugrippe.com/rss',
+     'http://lancemannion.typepad.com/lance_mannion/rss.xml',
      ]
    with open(f'feeds.txt', 'w', encoding='utf8') as f:
      f.write(",".join(subscriptions).replace(',', '\n'))
    return NFS_init.run()
-"""
-# check template if true
+
 """Then initialize code."""
-exist_template()
+entry_point()
