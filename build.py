d0d8cc32817c1d3b6b9cc60463fbdf96bddae117
Web Source (SRC) <web.system.management@gmail.com>
2021-04-18 23:00:01 +0300

    Update build.py

--- build.py
+++ build.py
@@ -1,13 +1,11 @@
 # build.py
 import subprocess
-from NFSyndication import __main__ as NFS_init
+from NFSyndication import init as NFS_init
 from NFSyndication import styles
 
 def entry_point():
    """ We use these conditions to check the statement"""
    subscriptions = [
-     'http://feeds.feedburner.com/jblanton',
-     'http://feeds.feedburner.com/IgnoreTheCode',
      'http://blog.ashleynh.me/feed',
      'https://www.betalogue.com/feed/',
      'https://bitsplitting.org/feed/',
