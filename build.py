2f968473cd98cef9f5986954b07e1082cb95cc30
Web Source (SRC) <web.system.management@gmail.com>
2021-04-18 23:02:47 +0300

    Update build.py

--- build.py
+++ build.py
@@ -9,10 +9,12 @@ def entry_point():
      'http://blog.ashleynh.me/feed',
      'https://www.betalogue.com/feed/',
      'https://bitsplitting.org/feed/',
+     'https://www.macstories.net/feed/'
+     'https://feedpress.me/candlerblog'
      ]
    with open(f'feeds.txt', 'w', encoding='utf8') as f:
      f.write(",".join(subscriptions).replace(',', '\n'))
-   return NFS_init.run()
+   return NFS_init()
 
 """Then initialize code."""
 entry_point()
